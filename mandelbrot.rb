131c52c6a569e28213aca3ff0c0c2383b4ed83e3
Hennau Pierre <pphennau@gmail.com>
2013-11-18 21:39:39 +0900

    fixing spacing

--- mandelbrot.rb
+++ mandelbrot.rb
@@ -3,34 +3,34 @@ require 'complex'
 
 class Mandelbrot < Processing::App
 
-	def setup
-		size(400, 400)
+  def setup
+    size(400, 400)
 
-		@step = 4/400.to_f
-		@infinity = 20
-	end
+    @step = 4/400.to_f
+    @infinity = 20
+  end
 
-	def draw
-		load_pixels
-		i = -2
-		(0...width).each do |x|
-			j = -2
-			(0...height).each do |y|
-				c = Complex(i, j)
-				z = Complex(0, 0)
-				k = 0
-				
-				while (z.abs < 2 && k < @infinity) do 
-					z = z**2 + c
-					k += 1
-				end
+  def draw
+    load_pixels
+    i = -2
+    (0...width).each do |x|
+       j = -2
+      (0...height).each do |y|
+        c = Complex(i, j)
+        z = Complex(0, 0)
+        k = 0
 
-				pixels[y*width + x] = color(0, 0, map(k, 0, 20, 0, 255))
-				j += @step
-			end
-			i += @step
-		end
-		update_pixels
-	end	
+        while (z.abs < 2 && k < @infinity) do
+          z = z**2 + c
+          k += 1
+        end
+
+        pixels[y*width + x] = color(0, 0, map(k, 0, 20, 0, 255))
+        j += @step
+      end
+      i += @step
+    end
+    update_pixels
+  end
 
 end
